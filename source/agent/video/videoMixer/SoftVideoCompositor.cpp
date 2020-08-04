11fb2c2b26e41471849461ad60b03395bc168b6c
duanchao <duanchao@spap.com>
2020-08-04 16:57:46 +0800

    11

--- source/agent/video/videoMixer/SoftVideoCompositor.cpp
+++ source/agent/video/videoMixer/SoftVideoCompositor.cpp
@@ -454,12 +454,12 @@ void SoftFrameGenerator::layout_regions(SoftFrameGenerator *t, rtc::scoped_refpt
         boost::shared_ptr<webrtc::VideoFrame> inputFrame = t->m_owner->getInputFrame(it->input);
 
         //start draw-text
-        rtc::scoped_refptr<webrtc::VideoFrameBuffer> compositeBuffer = inputFrame->video_frame_buffer();
-        webrtc::VideoFrame compositeFrame(
-                            compositeBuffer,
-                            webrtc::kVideoRotation_0,
-                            m_clock->TimeInMilliseconds()
-                            );
+//        rtc::scoped_refptr<webrtc::VideoFrameBuffer> compositeBuffer = inputFrame->video_frame_buffer();
+//        webrtc::VideoFrame compositeFrame(
+//                            compositeBuffer,
+//                            webrtc::kVideoRotation_0,
+//                            m_clock->TimeInMilliseconds()
+//                            );
 //		compositeFrame.set_timestamp(compositeFrame.timestamp_us() * kMsToRtpTimestamp);
 //
 //		owt_base::Frame frame;
