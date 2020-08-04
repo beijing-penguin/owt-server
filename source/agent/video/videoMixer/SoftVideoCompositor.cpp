95badc4054384845187c6eea9e71cbaa3486cddf
duanchao <duanchao@spap.com>
2020-08-04 16:17:26 +0800

    c++添加个人流draw 开始测试

--- source/agent/video/videoMixer/SoftVideoCompositor.cpp
+++ source/agent/video/videoMixer/SoftVideoCompositor.cpp
@@ -707,6 +707,32 @@ boost::shared_ptr<webrtc::VideoFrame> SoftVideoCompositor::getInputFrame(int ind
     auto& input = m_inputs[index];
     if (input->isActive()) {
         src = input->popInput();
+
+        //----------------------------start draw_text----------------------------
+        //TODO 添加文本
+        rtc::scoped_refptr<webrtc::VideoFrameBuffer> compositeBuffer = src->video_frame_buffer();
+        webrtc::VideoFrame compositeFrame(
+                            compositeBuffer,
+                            webrtc::kVideoRotation_0,
+                            m_clock->TimeInMilliseconds()
+                            );
+
+        compositeFrame.set_timestamp(compositeFrame.timestamp_us() * kMsToRtpTimestamp);
+
+		owt_base::Frame frame;
+		memset(&frame, 0, sizeof(frame));
+		frame.format = owt_base::FRAME_FORMAT_I420;
+		frame.payload = reinterpret_cast<uint8_t*>(&compositeFrame);
+		frame.length = 0; // unused.
+		frame.timeStamp = compositeFrame.timestamp();
+		frame.additionalInfo.video.width = compositeFrame.width();
+		frame.additionalInfo.video.height = compositeFrame.height();
+
+		m_textDrawer->drawFrame(frame);
+
+		src = reinterpret_cast<webrtc::VideoFrame>(frame.payload);
+        //boost::shared_ptr<webrtc::VideoFrame> frame(new webrtc::VideoFrame(i420Buffer, webrtc::kVideoRotation_0, 0));
+        //----------------------------end draw_text----------------------------
     } else {
         src = m_avatarManager->getAvatarFrame(index);
     }
