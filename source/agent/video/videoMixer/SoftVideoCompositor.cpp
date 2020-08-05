8e1b9297b62ce22bea1ad81664a9c305ae1858c4
duanchao <duanchao@spap.com>
2020-08-05 12:17:58 +0800

    dd

--- source/agent/video/videoMixer/SoftVideoCompositor.cpp
+++ source/agent/video/videoMixer/SoftVideoCompositor.cpp
@@ -470,7 +470,8 @@ void SoftFrameGenerator::layout_regions(SoftFrameGenerator *t, rtc::scoped_refpt
 		}else{
 			ELOG_INFO("write yuv file");
 		}
-		ELOG_INFO("height=%d",inputBuffer->height(),"width=%d",inputBuffer->width());
+		ELOG_INFO("width=%d",inputBuffer->width());
+		ELOG_INFO("height=%d",inputBuffer->height());
 		if (fp != NULL) {
 			fwrite(inputBuffer->DataY(), 1, inputBuffer->height() * inputBuffer->width(), fp);
 			fwrite(inputBuffer->DataU(), 1, inputBuffer->height() * inputBuffer->width() / 4, fp);
