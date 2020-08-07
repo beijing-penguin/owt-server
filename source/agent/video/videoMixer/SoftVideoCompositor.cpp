697300b1954864aa44126be87db577b46e9d7385
duanchao <duanchao@spap.com>
2020-08-07 14:53:00 +0800

    dd

--- source/agent/video/videoMixer/SoftVideoCompositor.cpp
+++ source/agent/video/videoMixer/SoftVideoCompositor.cpp
@@ -455,7 +455,6 @@ void SoftFrameGenerator::layout_regions(SoftFrameGenerator *t, rtc::scoped_refpt
     for (LayoutSolution::const_iterator it = regions.begin(); it != regions.end(); ++it) {
     	ELOG_INFO("it->input=%d!",it->input);
     	ELOG_INFO("t->m_owner->m_inputs[it->input]=%d!",t->m_owner->m_inputs[it->input]);
-    	t->m
         boost::shared_ptr<webrtc::VideoFrame> inputFrame = t->m_owner->getInputFrame(it->input);
         if (inputFrame == NULL) {
             continue;
