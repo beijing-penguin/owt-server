05d9d6f207ca7a32b34bab18ce511dfb590112f7
duanchao <duanchao@spap.com>
2020-08-10 10:18:32 +0800

    dd

--- source/agent/video/videoMixer/SoftVideoCompositor.cpp
+++ source/agent/video/videoMixer/SoftVideoCompositor.cpp
@@ -495,19 +495,20 @@ void SoftFrameGenerator::layout_regions(SoftFrameGenerator *t, rtc::scoped_refpt
     	//rtc::scoped_refptr<webrtc::VideoFrameBuffer> inputBuffer = t->m_owner->getInputFrame(it->input);
 
         // Cube - draw mark text - begin
-        char drawtext_dir[100];
-        char suffix[50] = ".drawtext";
-        sprintf(drawtext_dir,"./drawtext/%d%s",it->input,suffix);
+//        char drawtext_dir[100];
+//        char suffix[50] = ".drawtext";
+//        sprintf(drawtext_dir,"./drawtext/%d%s",it->input,suffix);
 
         std::string framedrawtext = t->m_owner->m_avatarManager->getMyFramedrawtext(it->input);
-        ELOG_INFO("framedrawtext======%s",framedrawtext.c_str());
-        if (!access(drawtext_dir,0) ){
-        	ELOG_INFO("drawtext_dir=%s EXISITS!",drawtext_dir);
-        	t->markFrame(inputBuffer, index++,it->input,framedrawtext);
-
-        }else{
-        	ELOG_INFO("drawtext_dir=%s DOESN'T EXISITS!",drawtext_dir);
-        }
+        //ELOG_INFO("framedrawtext======%s",framedrawtext.c_str());
+        t->markFrame(inputBuffer, index++,it->input,framedrawtext);
+//        if (!access(drawtext_dir,0) ){
+//        	ELOG_INFO("drawtext_dir=%s EXISITS!",drawtext_dir);
+//
+//
+//        }else{
+//        	ELOG_INFO("drawtext_dir=%s DOESN'T EXISITS!",drawtext_dir);
+//        }
         // Cube - draw mark text - end
 
         Region region = it->region;
