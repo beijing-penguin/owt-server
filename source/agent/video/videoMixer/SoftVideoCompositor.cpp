9c6188c788b317974ef9e103140eb521ac183c28
duanchao <duanchao@spap.com>
2020-08-13 14:59:44 +0800

    dd

--- source/agent/video/videoMixer/SoftVideoCompositor.cpp
+++ source/agent/video/videoMixer/SoftVideoCompositor.cpp
@@ -483,7 +483,10 @@ void SoftFrameGenerator::layout_regions(SoftFrameGenerator *t, rtc::scoped_refpt
 //        sprintf(drawtext_dir,"./drawtext/%d%s",it->input,suffix);
 
         std::string framedrawtext = t->m_owner->m_avatarManager->getMyFramedrawtext(it->input);
+
+        ELOG_INFO("layout_regions.framedrawtext========%s",framedrawtext.c_str());
         if(!framedrawtext.c_str()){
+        	ELOG_INFO("join if===layout_regions.framedrawtext========%s",framedrawtext.c_str());
         	t->markFrame(inputBuffer, index++,it->input,framedrawtext);
         }
         //ELOG_INFO("framedrawtext======%s",framedrawtext.c_str());
