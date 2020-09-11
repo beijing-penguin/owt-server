7549a2c1d4599a57861435f405dfdaa1bdc34be2
beijing-penguin <429544557@qq.com>
2020-09-11 15:33:06 +0800

    dd

---
+++ scripts/release/init-config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright (C) <2019> Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+
+bin=`dirname "$0"`
+echo $bin
+bin=`cd "$bin"; pwd`
+echo $bin
+ROOT=`cd "${bin}/.."; pwd`
+echo $ROOT
+usage()
+{
+  echo "Usage: [--deps] [--hardware] (Default: without deps and hardware)"
+}
